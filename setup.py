26a7d4ed331322897a8180efaf8bfbb98f095928
bryanthayes <bryantfhayes@gmail.com>
2017-04-09 21:21:53 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(name='coglint',
+      version='1.0.0',
+      py_modules=['coglint'],
+      # generate platform specific start script
+      entry_points={
+        'console_scripts': [
+            'coglint = coglint:main'
+        ]
+      },
+      install_requires=[],
+      url="http://en.wikipedia.org/wiki/coglint",
+      download_url="https://github.com/bryantfhayes/coglint",
+      keywords=["lint", "python", "c++"],
+      maintainer = 'Bryant Hayes',
+      maintainer_email = 'bryantfhayes@gmail.com',
+      classifiers=["Programming Language :: Python",
+                   "Programming Language :: Python :: 2",
+                   "Programming Language :: Python :: 2.6",
+                   "Programming Language :: Python :: 2.7",
+                   "Programming Language :: Python :: 3",
+                   "Programming Language :: Python :: 3.2",
+                   "Programming Language :: Python :: 3.3",
+                   "Programming Language :: Python :: 3.4",
+                   "Programming Language :: Python :: 3.5",
+                   "Programming Language :: C++",
+                   "Development Status :: 5 - Production/Stable",
+                   "Environment :: Console",
+                   "Topic :: Software Development :: Quality Assurance",
+                   "License :: Freely Distributable"],
+      description="An automated checker to make sure a C++ file follows the Cognex C++ style guide",
+      long_description=open('README.md').read())
