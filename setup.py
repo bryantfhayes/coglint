1d84fd2472d9a6ea0dd943b192e51cfacd18b432
Bryant Hayes <Bryant.Hayes@cognex.com>
2017-04-13 11:03:07 -0700

    Update version to v1.0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='coglint',
-      version='1.0.2',
+      version='1.0.3',
       py_modules=['coglint'],
       # generate platform specific start script
       entry_points={
